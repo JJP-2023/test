5be3da17158b16ec7ac61c18c2d899c5e1e3c5c8
JJP-2023 <145542091+JJP-2023@users.noreply.github.com>
2023-09-20 21:48:46 +0800

    上传一个.c文件
    第一次使用GitHub上传文件

---
+++ test.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/*int f(int a,int b)
+{
+	//int a=6,b=3;
+	if(a<b)
+		return (a,b);
+	else 
+		return (b,a); 
+}*/
+/*void f(void)
+{
+	printf("H");
+}*/
+/*struct person
+{
+	int num;
+	char name[20],sex;
+	struct{ int class;
+			char prof [20];
+	}in;
+} a={20,"Li Ming",'M',{5,"computer"}},*p=&a;
+*/
+int main(){
+	int i = 4;
+	i==4?printf("%d\n",i):printf("%d",i);
+	//printf("%s",p->name);
+//	f(void);
+	/*char c=256;
+	int a=c;
+	printf("%d",a);*/
+	return; 
+/*	double p=1.1;
+	p++;
+	printf("%f",p);*/
+	//int a[] = {1,2,3,4,5,6}, *p=a;
+	//printf("%d\n",(*++p)++);
+	//char c1='1',c2='\xff';
+	//float x=12.23232;
+	//printf("%f\n",x);
+	
+	//printf("%d\n",(int)(x*100+0.5)/100.0);
+	//x = (x*100+0.5)/100.0;
+	//printf("%f\n",x);
+	//c1 = getchar();
+	//c2 = getchar();
+	
+	//putchar(c1);
+	//putchar(c2);
+	//printf("%c",c2);
+	//return;
+} 
