4021d0e909b79d8a3f0ccdb981910d9670efb84c
JJP-2023 <145542091+JJP-2023@users.noreply.github.com>
2023-09-23 21:20:06 +0800

    上传非同名文件

---
+++ test2023312.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define S(x) x*x
+#define T(x) S(x)*(x)
+int main()
+{
+	
+	int i=5,j=2;
+	//i=C*L;
+	//int a = (i++)++;
+	char p[]={'a','b','c'};
+	char c = 256;
+	int a=c;
+	
+	//printf("%d",T(i+j));
+	printf("%d",a);
+	return;
+} 
